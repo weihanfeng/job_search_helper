84b31c33684a3db8248200a5fad1b81ec241b21d
wh_feng <weihan.feng.contact@gmial.com>
2023-06-10 12:35:50 +0800

    Add flask app

---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import pandas as pd
+from main import main
+
+app = Flask(__name__)
+
+def get_top_rows(job_title, num_pages, conditions, top_k):
+    # df = main(job_title, num_pages, conditions=conditions, top_k=top_k)
+    df = pd.read_csv("top_jobs.csv")
+    df = df.head(top_k)  # Filter top k rows
+    df['url'] = df['url'].apply(lambda x: f'<a href="{x}">{x}</a>')
+    return df
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        job_title = request.form['job_title']
+        num_pages = int(request.form['num_pages'])
+        top_k = int(request.form['top_k'])
+        job_levels = request.form.getlist('job_levels[]')  # Retrieve selected job levels as a list
+        df = get_top_rows(job_title, num_pages, job_levels, top_k)
+        return render_template('table.html', data=df)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
